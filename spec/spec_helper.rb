d5a8bb57c9b67a06ffdcce6640d8503fadd12961
Paul Osman <paul@eval.ca>
2015-05-12 18:53:50 -0400

    We never want to allow net connections from tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,14 @@
 require 'webmock/rspec'
 
+webmock_opts = {allow_localhost: true}
+
 if ENV['CODECLIMATE_REPO_TOKEN']
   require 'codeclimate-test-reporter'
   CodeClimate::TestReporter.start
-  WebMock.disable_net_connect!(:allow => "codeclimate.com")
+  webmock_opts[:allow] = 'codeclimate.com'
 end
+
+WebMock.disable_net_connect!(webmock_opts)
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'activite'
