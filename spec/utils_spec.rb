444460e88cf61b6afff539d164c5f813d4cb703e
Paul Osman <paul@eval.ca>
2015-05-12 21:30:48 -0400

    Add tests to utils

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -4,7 +4,9 @@ describe Activite::Utils do
 
   let (:startdateymd) { Hash['startdateymd', '2012-01-01'] }
   let (:startdate)    { Hash['startdate',    1325376000 ] }
-  
+  let (:enddateymd)   { Hash['enddateymd',   '2015-05-12'] }
+  let (:enddate)      { Hash['enddate',      1431388800 ] }
+
   describe '.normalize_date_params' do
     context 'when given a startdate in ymd format' do
       let (:opts) { subject.normalize_date_params(startdateymd) }
@@ -12,9 +14,21 @@ describe Activite::Utils do
       it 'preserves startdateymd' do
         expect(opts['startdateymd']).to eq(startdateymd['startdateymd'])
       end
-      
+
       it 'copies the value to startdate as a timestamp' do
-        expect(opts['startdate']).to eq(1325376000)
+        expect(opts['startdate']).to eq(startdate['startdate'])
+      end
+    end
+
+    context 'when given an enddate in ymd format' do
+      let (:opts) { subject.normalize_date_params(enddateymd) }
+
+      it 'preserves enddateymd' do
+        expect(opts['enddateymd']).to eq(enddateymd['enddateymd'])
+      end
+
+      it 'copies the value to enddate as a timestamp' do
+        expect(opts['enddate']).to eq(enddate['enddate'])
       end
     end
 
@@ -30,11 +44,11 @@ describe Activite::Utils do
       end
     end
 
-    context 'when given a Date instance' do      
+    context 'when given a Date instance' do
       context 'as a startdateymd param' do
         let(:startdateymd) { Hash['startdateymd', Date.new(2012, 01, 01)] }
         let(:opts) { subject.normalize_date_params(startdateymd) }
-        
+
         it 'converts the Date instance into YYYY-MM-DD format' do
           expect(opts['startdateymd']).to eq('2012-01-01')
         end
